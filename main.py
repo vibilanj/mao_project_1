cf63bd32d3e31a9cb8162755089f057b88b2a500
Vibilan Jayanth <vibilan@firstindo.com>
2024-02-04 12:49:44 +0800

    used kruskal and improved drawing of graphs

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+from kruskal import kruskal, print_mst
 
-random.seed(1) #TODO: remove later
+random.seed(1) # TODO: remove seed
+
+# Generation parameters
+n = 10
+p = 0.25
+# TODO: add more parameters for weight ranges, node color, edge color, etc.
 
 # Generating random connected graphs
-p = 0.25
-G = nx.gnp_random_graph(10, p, seed=1) #TODO: remove seed
+G = nx.gnp_random_graph(n, p, seed = 1) # TODO: remove seed
 while not nx.is_connected(G):
-    G = nx.gnp_random_graph(10, p)
+    G = nx.gnp_random_graph(n, p)
 
+# Adding random weights to the edges
 for (u, v) in G.edges():
-    G.edges[u, v]['weight'] = random.randint(1, 10)
+    G.edges[u, v]["weight"] = random.randint(1, 10)
+    G.edges[u, v]["color"] = "black"
 
-# print(G.edges(data=True))
+def draw_graph_to_file(filename):
+    pos = nx.circular_layout(G)
+    color = [G.edges[u, v]["color"] for (u, v) in G.edges()]
+    nx.draw(G, with_labels = True, pos = pos, edge_color = color)
+    
+    for i, e in enumerate(G.edges(data = True)):
+        nx.draw_networkx_edge_labels(G, pos,
+                                     edge_labels = {(e[0], e[1]): e[2]["weight"]},
+                                     font_color = color[i])
+        
+    plt.savefig(filename)
 
-pos = nx.circular_layout(G)
-nx.draw(G, with_labels=True, pos=pos)
-labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+# Drawing the question graph
+draw_graph_to_file("graph.png")
 
-plt.savefig("graph.png")
+# Finding the minimum spanning tree for the graph
+mst = kruskal(n, G.edges(data = True))
+# nx.write_edgelist(G, "graph.edgelist")
+print_mst(mst)
 
-nx.write_edgelist(G, "graph.edgelist")
+# Drawing the solution graph
+for (u, v, w) in mst:
+    G.edges[u, v]["color"] = "red"
+draw_graph_to_file("graph_mst.png")
